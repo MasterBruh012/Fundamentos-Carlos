3842dc19133144855aac12e7de118a74955c431a
Carlos Hernández <manzana39825@gmail.com>
2023-05-28 19:38:22 -0400

    Ejercicios 12

--- Ejercicios capitulo 12/12.4/12,4.c
+++ Ejercicios capitulo 12/12.4/12,4.c
@@ -1,6 +1,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 3
+#define M 3
+
 int main()
 {
 
@@ -15,7 +18,6 @@ int main()
 
     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓
     */
-
 
     /*
     int *puntarr;                              // puntero unidimensional.
@@ -33,23 +35,51 @@ int main()
     return 0;
     */
 
-
     /*
     En este siguiente ejemplo, podemos ver como crear un puntero bidimensional
     utilizando aritmética, asignandole valores y moviendonos dentro del arreglo.
 
+    La primera forma, solamente creamos un puntero para las filas
     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓
     */
 
-    int (*bipuntarr)[3];                              // Creamos un puntero para las filas.
-    bipuntarr = malloc(3 * sizeof(*bipuntarr));       // Asignamos 3 espacios al puntero de las filas.
+    /*
+    int(*filpuntarr)[3];                         // Creamos un puntero para las filas.
+    filpuntarr = malloc(3 * sizeof(*filpuntarr)); // Asignamos 3 espacios al puntero de las filas.
 
     for (int i = 0; i < 3; i++)
-    {   
+    {
         for (int j = 0; j < 3; j++)
         {
-            bipuntarr[i][j]=i+j;
-            printf(" %d ", bipuntarr[i][j]);
+            filpuntarr[i][j] = i + j;
+            printf(" %d ", filpuntarr[i][j]);
+        }
+
+        printf("\n");
+    }
+
+    free(filpuntarr);
+
+    return 0;
+    */
+
+
+    /*
+    La segunda forma, creamos un solo punteor que manejará
+    las filas y las columnas.
+    ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓     ↓
+    */
+
+    int *bipuntarr;                                 // Creamos un puntero.
+    bipuntarr = (int*) malloc(N * M * sizeof(int)); // Asignamos los espacios de las filas y las colimnas.
+
+    // Accedemos a los elementos utilizando arimética de punteros.
+    for (int i = 0; i < N; i++)
+    {
+        for (int j = 0; j < M; j++)
+        {
+            *(bipuntarr + i * M + j) = i + j; //Asignamos valores a los elementos.
+            printf(" %d ", *(bipuntarr + i * M + j)); // imprimimos los valores.
         }
 
         printf("\n");
--- Ejercicios capitulo 12/12.5/12,5.c
+++ Ejercicios capitulo 12/12.5/12,5.c
@@ -1,9 +1,63 @@
 #include <stdio.h>
 #include <stdlib.h>
 
+#define N 4
+#define M 5
+
+/*
+En el siguiente código se accede a los elementos
+de una matriz. Acceder a los mismos elementos
+con aritmética de punteros.
+*/
+
+/*
+Código original.
+↓       ↓       ↓
+*/
+
+/*
 int main()
 {
+    int f, c;
+    double mt[N][M];
 
+    for (f = 0; f < N; f++)
+    {
+        for (c = 0; c < M; c++)
+        {
+            mt[f][c] = f + c;
+            printf(" %lf ", mt[f][c]);
+        }
+
+        printf("\n");
+    }
+
+    return 0;
+
+}
+*/
+
+/*
+Código con punteros.
+↓       ↓       ↓
+*/
+
+int main()
+{
+    int f, c;
+    double *puntero;                                    // Creamos un puntero.
+    puntero = (double *)malloc(N * M * sizeof(double)); // Asignamos el tamaño de las filas y columnas al puntero.
+
+    for (f = 0; f < N; f++)
+    {
+        for (c = 0; c < M; c++)
+        {
+            *(puntero + f * M + c) = f + c;          // Acceder a los valores del puntero y llenarlos.
+            printf(" %lf ", *(puntero + f * M + c)); // Imprimir los valores del puntero.
+        }
+
+        printf("\n");
+    }
 
     return 0;
 }
